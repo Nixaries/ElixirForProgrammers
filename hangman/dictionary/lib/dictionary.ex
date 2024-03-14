0a97379c748e2329024d4c85b5cd909e12fa4e0e
dannyinplaysports <danny@in-playsports.com>
2024-03-14 08:51:32 +0000

    Basic Dictionary

---
+++ hangman/dictionary/lib/dictionary.ex
@@ -0,0 +1,10 @@
+defmodule Dictionary do
+  def word_list do
+    File.read!("assets/words.txt")
+    |> String.split(~r/\n/, trim: true)
+  end
+
+  def random_word do
+    Enum.random(word_list())
+  end
+end
